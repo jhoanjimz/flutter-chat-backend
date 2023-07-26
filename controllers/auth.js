3858fee5ba1e2dbd8e0d383ad561593841856ad4
jhoanjimz <jhoan.developer@gmail.com>
2023-07-26 13:15:33 -0500

    Fin sección 8

---
+++ controllers/auth.js
@@ -0,0 +1,111 @@
+const { response } = require('express');
+
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const { generarJWT } = require('../helpers/jwt');
+
+const crearUsuario = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+        
+        const existeEmail = await Usuario.findOne({ email });
+        if (existeEmail) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El correo ya está registrado'
+            })
+        }
+
+        const usuario = new Usuario(req.body);
+
+        //Encriptar Contraseña
+        const salt = bcrypt.genSaltSync();
+        usuario.password = bcrypt.hashSync(password, salt);
+
+        await usuario.save();
+
+        // Generar mi JWT
+        const token = await generarJWT(usuario.id)
+    
+        res.json({
+            ok: true,
+            usuario,
+            token
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+
+}
+
+
+const login = async(req, res = response) => {
+
+    const { email, password } = req.body;
+
+    try {
+
+        const usuarioDB = await Usuario.findOne({ email });
+        if (!usuarioDB) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Email no encontrado'
+            })
+        }
+
+        // Validar el password
+        const validPassword = bcrypt.compareSync(password, usuarioDB.password);
+        if ( !validPassword ) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'La contraseña no es valida'
+            })
+        }
+
+        // Generar JWT
+        const token = await generarJWT(usuarioDB.id);
+    
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token
+        })
+        
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            ok: false,
+            msg: 'Hable con el administrador'
+        })
+    }
+}
+
+const renewToken = async(req, res = response) => {
+
+    const uid = req.uid;
+
+    // Generar Nuevo JWT
+    const token = await generarJWT(uid);
+
+    // Obtener usuario por el UID
+    const usuario = await Usuario.findById( uid );
+    
+    res.json({
+        ok: true,
+        usuario,
+        token
+    })
+}
+
+
+module.exports = {
+    crearUsuario,
+    login,
+    renewToken
+}
